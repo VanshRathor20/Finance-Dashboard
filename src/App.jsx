c399158a76d8eba9808bdc9f6952fe7db17d2bae
VanshRathor20 <vanshrathor.personal@gmail.com>
2026-04-05 13:05:24 +0530

    components & pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Navigate, Outlet, Route, Routes, BrowserRouter } from "react-router-dom";
+import AppProvider from "./context/AppContext";
+import Navbar from "./components/layout/Navbar";
+import Sidebar from "./components/layout/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
+import Insights from "./pages/Insights";
+
+function AppLayout() {
+  return (
+    <div className="flex h-screen min-h-0 w-full">
+      <aside className="flex w-[220px] shrink-0 flex-col">
+        <Sidebar />
+      </aside>
+      <div className="flex min-h-0 min-w-0 flex-1 flex-col">
+        <header className="shrink-0">
+          <Navbar />
+        </header>
+        <main className="min-h-0 flex-1 overflow-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
 
 const App = () => {
   return (
-    <div className='bg-red-500 text-7xl'>App</div>
-  )
-}
+    <AppProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route element={<AppLayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/transactions" element={<Transactions />} />
+            <Route path="/insights" element={<Insights />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
+  );
+};
 
-export default App
+export default App;
